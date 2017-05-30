e971405db669c171d5004f1fa01cd3269131e311
Anwesh43 <anweshthecool0@gmail.com>
2017-05-31 02:57:48 +0530

    using foursquare view in app

--- app/src/main/java/com/anwesome/ui/foursquareviewdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/foursquareviewdemo/MainActivity.java
@@ -2,6 +2,9 @@ package com.anwesome.ui.foursquareviewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import com.anwesome.ui.foursquareview.FourSquareView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FourSquareView fourSquareView = new FourSquareView(this);
+        addContentView(fourSquareView,new ViewGroup.LayoutParams(600,600));
     }
 }
