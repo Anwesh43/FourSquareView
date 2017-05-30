509e4a0868b9a40b86aa5d517fb6e53dfe0f971e
Anwesh43 <anweshthecool0@gmail.com>
2017-05-31 02:43:18 +0530

    adding animation handler

--- foursquareview/src/main/java/com/anwesome/ui/foursquareview/FourSquareView.java
+++ foursquareview/src/main/java/com/anwesome/ui/foursquareview/FourSquareView.java
@@ -16,7 +16,6 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class FourSquareView extends View {
     private int time = 0,w,h;
-    private Square tappedSquare = null;
     private ConcurrentLinkedQueue<Square> squares = new ConcurrentLinkedQueue<>();
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private int firstColor = Color.parseColor("#009688"),secondColor = Color.parseColor("#00BCD4"),thirdColor = Color.parseColor("#1E88E5"),fourthColor = Color.parseColor("#f44336");
@@ -56,23 +55,17 @@ public class FourSquareView extends View {
         time++;
     }
     public boolean onTouchEvent(MotionEvent event) {
-        if(event.getAction() == MotionEvent.ACTION_DOWN && tappedSquare == null) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
             for(Square square:squares) {
                 if(square.handleTap(event.getX(),event.getY())) {
-                    tappedSquare = square;
+                    break;
                 }
             }
         }
         return true;
     }
-    public void update(float factor) {
-        if(tappedSquare != null) {
-            tappedSquare.update(factor);
-        }
-        postInvalidate();
-    }
     private class Square {
-        private float x,y,size,scale = 0;
+        private float x,y,size,scale = 0,dir = 0;
         private int color;
         public Square(float x,float y,int color) {
             this.x = x;
@@ -94,14 +87,46 @@ public class FourSquareView extends View {
             canvas.drawRect(new RectF(0,0,w,h),paint);
             canvas.restore();
         }
-        public void update(float factor) {
-            scale = factor;
+        public void update() {
+            scale += 0.2f*dir;
+            if(scale > 1) {
+                dir = 0;
+                scale = 1;
+            }
+            if(scale < 0) {
+                dir = 0;
+                scale = 0;
+            }
+        }
+        public boolean stopped() {
+            return dir == 0;
         }
         public boolean handleTap(float x,float y) {
-            return x>=this.x && x<=this.x+size && y>=this.y && y<=this.y+size;
+            return x>=this.x && x<=this.x+size && y>=this.y && y<=this.y+size && dir == 0;
         }
         public int hashCode() {
             return (int)(x+y+color);
+        }
+        public float getScale() {
+            return scale;
+        }
+    }
+    private class AnimationHandler {
+        private ConcurrentLinkedQueue<Square> tappedSquares = new ConcurrentLinkedQueue<>();
+        public void animate() {
+            for(Square square:tappedSquares) {
+                square.update();
+            }
+            try {
+                Thread.sleep(100);
+                invalidate();
+            }
+            catch (Exception ex) {
+
+            }
+        }
+        public void addSquare(Square square) {
+            tappedSquares.add(square);
         }
     }
 }
